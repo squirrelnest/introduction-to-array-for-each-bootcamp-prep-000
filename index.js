599e36f8b933f5ec80dbf7f7f3fd335e1b358e91
Mikka Pineda <mikkanthrope@gmail.com>
2017-04-09 05:51:14 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,9 @@
+function doToElementsInArray(array, callback) {
+  array.forEach(callback)
+}
+
+function changeCompletely(element, index, array) {
+  array[index] = (Math.random() * 100).toString() + '!!!'
+}
+ 
+var animals = ["dog", "fish", "cat"]
